d6803599048e60f9feac5443e5fa291f3fff4fa3
Dániel Fodor <daniel.fodor@semilab.hu>
2024-09-16 17:44:22 +0200

    the bug part

--- GetViewbug/Modules/GetViewbug.Modules.ModuleName/ModuleNameModule.cs
+++ GetViewbug/Modules/GetViewbug.Modules.ModuleName/ModuleNameModule.cs
@@ -3,6 +3,7 @@ using GetViewbug.Modules.ModuleName.Views;
 using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
+using System;
 
 namespace GetViewbug.Modules.ModuleName
 {
@@ -17,14 +18,20 @@ namespace GetViewbug.Modules.ModuleName
 
         public void OnInitialized(IContainerProvider containerProvider)
         {
+            //Registering like this should name the ItemMetadataCollection[0].Name right ?
             _regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, "ViewA");
-            var thisisNull = _regionManager.Regions[RegionNames.ContentRegion].GetView("ViewA");
-            _regionManager.RequestNavigate(RegionNames.ContentRegion, "ViewA");
+
+            //but this still returns null
+            var thisShouldNotBeNull = _regionManager.Regions[RegionNames.ContentRegion].GetView("ViewA");
+
+            if (thisShouldNotBeNull is null)
+                throw new InvalidOperationException("_regionManager.Regions[RegionNames.ContentRegion].GetView(\"ViewA\")  returned null");
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            containerRegistry.RegisterForNavigation<ViewA>("ViewA");
+            //naming it here does not help either
+            containerRegistry.RegisterForNavigation<ViewA>(/*ViewA*/);
         }
     }
 }
